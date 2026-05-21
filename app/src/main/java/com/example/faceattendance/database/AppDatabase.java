6e159aaaf123d845b6af2f04b35b571c2ed7fbba
Quan <kakasumata2@gmail.com>
2026-05-21 10:28:30 +0700

    sua lai ket noi database

--- app/src/main/java/com/example/faceattendance/database/AppDatabase.java
+++ app/src/main/java/com/example/faceattendance/database/AppDatabase.java
@@ -102,11 +102,22 @@ public abstract class AppDatabase extends RoomDatabase {
                                     AppDatabase.class,
                                     "face_attendance.db"
                             )
-                            // ← DÙNG FILE DB TỪ ASSETS
-                            .createFromAsset("face_attendance.db")
-                            .allowMainThreadQueries()
-                            .fallbackToDestructiveMigration()
+                            .addMigrations(
+                                    MIGRATION_1_2,
+                                    MIGRATION_2_3,
+                                    MIGRATION_3_4,
+                                    MIGRATION_4_5,
+                                    MIGRATION_5_6,
+                                    MIGRATION_6_7,
+                                    MIGRATION_7_8
+                            )
                             .build();
+
+                            // ← DÙNG FILE DB TỪ ASSETS
+                            //.createFromAsset("face_attendance.db")
+                            //.allowMainThreadQueries()
+                            //.fallbackToDestructiveMigration()
+                            //.build();
                 }
             }
         }
